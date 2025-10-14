04b9525efe0d1f8c64bb6e211bd7bef779ffcef2
chomnan2168-0925 <chomnan2168@gmail.com>
2025-10-14 21:19:30 +0700

    Update index.tsx

--- pages/admin0925/index.tsx
+++ pages/admin0925/index.tsx
@@ -2,11 +2,10 @@
 
 import React from 'react';
 import Dashboard from '@/components/admin/Dashboard';
+import withAuth from '@/components/auth/withAuth';
 
 const AdminDashboardPage = () => {
-  // The page now ONLY returns its direct content.
-  // The AdminLayout is now provided by _app.tsx.
   return <Dashboard />;
 };
 
-export default AdminDashboardPage;
+export default withAuth(AdminDashboardPage);
